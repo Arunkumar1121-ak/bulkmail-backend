81d1817e1b645c52ce72cb71bc16f38132a1e60e
Arunkumar1121-ak <ktmarunkumar143@gmail.com>
2025-09-19 12:41:08 +0530

    fix: updated backend for Render env variables

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const nodemailer = require("nodemailer");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://ak:143@cluster0.waxcn9c.mongodb.net/passkey?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("db connected");
     })
@@ -64,6 +69,8 @@ app.post("/sendmail", (req, res) => {
             res.send(false);
         });
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () => {
     console.log("server is started.....");
