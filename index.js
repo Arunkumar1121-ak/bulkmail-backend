174d129d18a18e1bfabff551866bb9850a7f58bf
Arunkumar1121-ak <ktmarunkumar143@gmail.com>
2025-09-19 13:11:49 +0530

    fix: updated backend for Render env variables

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ app.post("/sendmail", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("server is started.....");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
